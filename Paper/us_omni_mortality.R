d91334d721390be1e31dacb2bbb8c5443baa9af3
Soryan Kumar <47063482+Soryan-Kumar@users.noreply.github.com>
2022-07-10 18:06:40 -0400

    Update us_omni_mortality.R

--- Paper/us_omni_mortality.R
+++ Paper/us_omni_mortality.R
@@ -1,7 +1,7 @@
 #estimate US omicron mortality (1/7-4/7)
 
-omi_mort_1_7 <- read.csv("~/Projects/Research/Mihir Projects/COVID-19 Model/omicron_deaths_1_7.csv", stringsAsFactors = FALSE)
-omi_mort_4_7 <- read.csv("~/Projects/Research/Mihir Projects/COVID-19 Model/omicron_deaths_4_7.csv", stringsAsFactors = FALSE)
+omi_mort_1_7 <- read.csv("../data/omicron_deaths_1_7.csv", stringsAsFactors = FALSE)
+omi_mort_4_7 <- read.csv("../data/omicron_deaths_4_7.csv", stringsAsFactors = FALSE)
 
 #filter out non-US areas (keep NYC,DC)
 omi_mort_1_7 <- subset(omi_mort_1_7,!(state %in% c("AS","FSM","GU","MP","PR","PW","RMI","VI")))
