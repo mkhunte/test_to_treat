2d2f56777dda8c3aebe426854e4e0cc9c23f6446
Soryan Kumar <47063482+Soryan-Kumar@users.noreply.github.com>
2022-07-10 19:16:13 -0400

    Update nh_pax_uptake_apr_may.R

--- Paper/nh_pax_uptake_apr_may.R
+++ Paper/nh_pax_uptake_apr_may.R
@@ -1,5 +1,5 @@
 #compute nursing home uptake over april and may
-nursing_data <- read.csv("~/Projects/Research/Mihir Projects/COVID-19 Model/COVID-19 Nursing Home Data 06.26.2022.csv", stringsAsFactors = FALSE)
+nursing_data <- read.csv("../data/COVID-19 Nursing Home Data 06.26.2022.csv", stringsAsFactors = FALSE)
 
 #impute 0 for missing therapeutic data (over April and May)
 nurse_impute_apr_may <- subset(nursing_data,Week.Ending %in% c("04/03/22","04/10/22","04/17/22","04/24/22","05/01/22","05/08/22","05/15/22","05/22/22","05/29/22"))
