bacc578b32a9a538fb37fc97950e0fe921a13c74
mkhunte <109043320+mkhunte@users.noreply.github.com>
2022-07-10 17:29:28 -0400

    Add files via upload

---
+++ Paper/total_infections_Apr22_Apr28.R
@@ -0,0 +1,9 @@
+
+data22_org <- read.csv("covid19_infection_estimate_2022.csv")
+data22 <- data22_org[data22_org$location_name == 'United States of America', ]
+
+data22$date <- as.Date(data22$date)
+
+data22_April <- data22[data22$date >= "2022-4-22" & data22$date <= "2022-4-28", ]
+
+sum(data22_April$inf_mean)
